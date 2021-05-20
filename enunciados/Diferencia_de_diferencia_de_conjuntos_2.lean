a7e7baa56567a996b4e27af38c7a8fc8cf8e2667
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-05-20 13:37:11 +0200

    enunciados/Diferencia_de_diferencia_de_conjuntos_2.lean

---
+++ enunciados/Diferencia_de_diferencia_de_conjuntos_2.lean
@@ -0,0 +1,13 @@
+-- -----------------------------
+-- Demostrar que
+--    s \ (t ∪ u) ⊆ (s \ t) \ u
+-- -----------------------------
+
+import data.set.basic
+open set
+
+variable {α : Type}
+variables s t u : set α
+
+example : s \ (t ∪ u) ⊆ (s \ t) \ u :=
+sorry
