a14e5ef8af73f4ea5c203737c34bbce708b59c1b
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-05-20 09:13:13 +0200

    Propiedad_semidistributiva_de_la_interseccion_sobre_la_union_2.lean

--- enunciados/Propiedad_semidistributiva_de_la_interseccion_sobre_la_union_2.lean
+++ enunciados/Propiedad_semidistributiva_de_la_interseccion_sobre_la_union_2.lean
@@ -1,13 +1,7 @@
--- Propiedad_semidistributiva_de_la_interseccion_sobre_la_union_2.lean
--- 2ª propiedad semidistributiva de la intersección sobre la unión
--- José A. Alonso Jiménez
--- Sevilla, 20 de mayo de 2021
--- ---------------------------------------------------------------------
-
--- ---------------------------------------------------------------------
+-- ----------------------------------
 -- Demostrar que
---    (s ∩ t) ∪ (s ∩ u) ⊆ s ∩ (t ∪ u
--- ----------------------------------------------------------------------
+--    (s ∩ t) ∪ (s ∩ u) ⊆ s ∩ (t ∪ u)
+-- ----------------------------------
 
 import data.set.basic
 open set
@@ -15,48 +9,5 @@ open set
 variable {α : Type}
 variables s t u : set α
 
--- 1ª demostración
--- ===============
-
 example : (s ∩ t) ∪ (s ∩ u) ⊆ s ∩ (t ∪ u):=
-begin
-  intros x hx,
-  cases hx with xst xsu,
-  { split,
-    { exact xst.1 },
-    { left,
-      exact xst.2 }},
-  { split,
-    { exact xsu.1 },
-    { right,
-      exact xsu.2 }},
-end
-
--- 2ª demostración
--- ===============
-
-example : (s ∩ t) ∪ (s ∩ u) ⊆ s ∩ (t ∪ u):=
-begin
-  rintros x (⟨xs, xt⟩ | ⟨xs, xu⟩),
-  { use xs,
-    left,
-    exact xt },
-  { use xs,
-    right,
-    exact xu },
-end
-
--- 3ª demostración
--- ===============
-
-example : (s ∩ t) ∪ (s ∩ u) ⊆ s ∩ (t ∪ u):=
-by rw inter_distrib_left s t u
-
--- 4ª demostración
--- ===============
-
-example : (s ∩ t) ∪ (s ∩ u) ⊆ s ∩ (t ∪ u):=
-begin
-  intros x hx,
-  finish
-end
+sorry
