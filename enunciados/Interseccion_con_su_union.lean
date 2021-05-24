322d97a907bef7674ae4006664f8e79f1a06c7f3
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-05-24 14:02:25 +0200

    enunciados/Interseccion_con_su_union.lean

---
+++ enunciados/Interseccion_con_su_union.lean
@@ -0,0 +1,13 @@
+-- ------------------
+-- Demostrar que
+--    s ∩ (s ∪ t) = s
+-- ------------------
+
+import data.set.basic
+open set
+
+variable {α : Type}
+variables s t : set α
+
+example : s ∩ (s ∪ t) = s :=
+sorry
