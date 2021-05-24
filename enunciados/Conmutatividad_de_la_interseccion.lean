480f7b6c96e9bb43f004beb92c931fc7b7f920f4
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-05-24 11:00:03 +0200

    enunciados/Conmutatividad_de_la_interseccion.lean

---
+++ enunciados/Conmutatividad_de_la_interseccion.lean
@@ -0,0 +1,13 @@
+-- ----------------
+-- Demostrar que
+--    s ∩ t = t ∩ s
+-- ----------------
+
+import data.set.basic
+open set
+
+variable {α : Type}
+variables s t u : set α
+
+example : s ∩ t = t ∩ s :=
+sorry
