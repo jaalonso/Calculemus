8e598391161116bbf14e7a147bcf387b00c2bf59
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-05-24 13:05:22 +0200

    Diferencia_de_diferencia_de_conjuntos_2.thy: Códigos

--- thy/Diferencia_de_diferencia_de_conjuntos_2.thy
+++ thy/Diferencia_de_diferencia_de_conjuntos_2.thy
@@ -19,106 +19,106 @@ lemma "s - (t \<union> u) \<subseteq> (s - t) - u"
 proof (rule subsetI)
   fix x
   assume "x \<in> s - (t \<union> u)"
-  then show "x \<in> (s - t) - u" 
+  then show "x \<in> (s - t) - u"
   proof (rule DiffE)
     assume "x \<in> s"
     assume "x \<notin> t \<union> u"
-    have "x \<notin> u" 
+    have "x \<notin> u"
     proof (rule notI)
       assume "x \<in> u"
-      then have "x \<in> t \<union> u" 
+      then have "x \<in> t \<union> u"
         by (simp only: UnI2)
       with \<open>x \<notin> t \<union> u\<close> show False
         by (rule notE)
-    qed 
-    have "x \<notin> t" 
+    qed
+    have "x \<notin> t"
     proof (rule notI)
       assume "x \<in> t"
-      then have "x \<in> t \<union> u" 
+      then have "x \<in> t \<union> u"
         by (simp only: UnI1)
       with \<open>x \<notin> t \<union> u\<close> show False
         by (rule notE)
-    qed 
-    with \<open>x \<in> s\<close> have "x \<in> s - t" 
+    qed
+    with \<open>x \<in> s\<close> have "x \<in> s - t"
       by (rule DiffI)
-    then show "x \<in> (s - t) - u" 
-      using \<open>x \<notin> u\<close> by (rule DiffI)  
+    then show "x \<in> (s - t) - u"
+      using \<open>x \<notin> u\<close> by (rule DiffI)
   qed
 qed
 
 (* 2\<ordfeminine> demostración *)
 lemma "s - (t \<union> u) \<subseteq> (s - t) - u"
-proof 
+proof
   fix x
   assume "x \<in> s - (t \<union> u)"
-  then show "x \<in> (s - t) - u" 
-  proof 
+  then show "x \<in> (s - t) - u"
+  proof
     assume "x \<in> s"
     assume "x \<notin> t \<union> u"
-    have "x \<notin> u" 
-    proof 
+    have "x \<notin> u"
+    proof
       assume "x \<in> u"
-      then have "x \<in> t \<union> u" 
+      then have "x \<in> t \<union> u"
         by simp
       with \<open>x \<notin> t \<union> u\<close> show False
         by simp
-    qed 
-    have "x \<notin> t" 
-    proof 
+    qed
+    have "x \<notin> t"
+    proof
       assume "x \<in> t"
-      then have "x \<in> t \<union> u" 
+      then have "x \<in> t \<union> u"
         by simp
       with \<open>x \<notin> t \<union> u\<close> show False
         by simp
-    qed 
-    with \<open>x \<in> s\<close> have "x \<in> s - t" 
+    qed
+    with \<open>x \<in> s\<close> have "x \<in> s - t"
       by simp
-    then show "x \<in> (s - t) - u" 
-      using \<open>x \<notin> u\<close> by simp  
+    then show "x \<in> (s - t) - u"
+      using \<open>x \<notin> u\<close> by simp
   qed
 qed
 
 (* 3\<ordfeminine> demostración *)
 lemma "s - (t \<union> u) \<subseteq> (s - t) - u"
-proof 
+proof
   fix x
   assume "x \<in> s - (t \<union> u)"
-  then show "x \<in> (s - t) - u" 
-  proof 
+  then show "x \<in> (s - t) - u"
+  proof
     assume "x \<in> s"
     assume "x \<notin> t \<union> u"
-    then have "x \<notin> u" 
-      by simp 
-    have "x \<notin> t" 
-      using \<open>x \<notin> t \<union> u\<close> by simp
-    with \<open>x \<in> s\<close> have "x \<in> s - t" 
+    then have "x \<notin> u"
       by simp
-    then show "x \<in> (s - t) - u" 
-      using \<open>x \<notin> u\<close> by simp  
+    have "x \<notin> t"
+      using \<open>x \<notin> t \<union> u\<close> by simp
+    with \<open>x \<in> s\<close> have "x \<in> s - t"
+      by simp
+    then show "x \<in> (s - t) - u"
+      using \<open>x \<notin> u\<close> by simp
   qed
 qed
 
 (* 4\<ordfeminine> demostración *)
 lemma "s - (t \<union> u) \<subseteq> (s - t) - u"
-proof 
+proof
   fix x
   assume "x \<in> s - (t \<union> u)"
-  then show "x \<in> (s - t) - u" 
-  proof 
+  then show "x \<in> (s - t) - u"
+  proof
     assume "x \<in> s"
     assume "x \<notin> t \<union> u"
-    then show "x \<in> (s - t) - u" 
-      using \<open>x \<in> s\<close> by simp  
+    then show "x \<in> (s - t) - u"
+      using \<open>x \<in> s\<close> by simp
   qed
 qed
 
 (* 5\<ordfeminine> demostración *)
 lemma "s - (t \<union> u) \<subseteq> (s - t) - u"
-proof 
+proof
   fix x
   assume "x \<in> s - (t \<union> u)"
-  then show "x \<in> (s - t) - u" 
-    by simp  
+  then show "x \<in> (s - t) - u"
+    by simp
 qed
 
 (* 6\<ordfeminine> demostración *)
