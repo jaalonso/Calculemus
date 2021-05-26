50cbdfe60375e145908921c580880c8ae2ec5218
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-05-26 13:47:16 +0200

    src/Union_con_su_diferencia.lean

---
+++ src/Union_con_su_diferencia.lean
@@ -0,0 +1,105 @@
+-- Union_con_su_diferencia.lean
+-- Unión con su diferencia
+-- José A. Alonso Jiménez
+-- Sevilla, 27 de mayo de 2021
+-- ---------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------
+-- Demostrar que
+--    (s \ t) ∪ t = s ∪ t
+-- ----------------------------------------------------------------------
+
+import data.set.basic
+open set
+
+variable {α : Type}
+variables s t : set α
+
+-- 1ª definición
+-- =============
+
+example : (s \ t) ∪ t = s ∪ t :=
+begin
+  ext x,
+  split,
+  { intro hx,
+    cases hx with xst xt,
+    { left,
+      exact xst.1, },
+    { right,
+      exact xt }},
+  { by_cases h : x ∈ t,
+    { intro _,
+      right,
+      exact h },
+    { intro hx,
+      cases hx with xs xt,
+      { left,
+        split,
+        { exact xs, },
+        { dsimp,
+          exact h, }},
+      { right,
+        exact xt, }}},
+end
+
+-- 2ª definición
+-- =============
+
+example : (s \ t) ∪ t = s ∪ t :=
+begin
+  ext x,
+  split,
+  { rintros (⟨xs, nxt⟩ | xt),
+    { left,
+      exact xs},
+    { right,
+      exact xt }},
+  { by_cases h : x ∈ t,
+    { intro _,
+      right,
+      exact h },
+    { rintros (xs | xt),
+      { left,
+        use [xs, h] },
+      { right,
+        use xt }}},
+end
+
+-- 3ª definición
+-- =============
+
+example : (s \ t) ∪ t = s ∪ t :=
+begin
+  rw ext_iff,
+  intro,
+  rw iff_def,
+  finish,
+end
+
+-- 4ª definición
+-- =============
+
+example : (s \ t) ∪ t = s ∪ t :=
+by finish [ext_iff, iff_def]
+
+-- 5ª definición
+-- =============
+
+example : (s \ t) ∪ t = s ∪ t :=
+diff_union_self
+
+-- 6ª definición
+-- =============
+
+example : (s \ t) ∪ t = s ∪ t :=
+begin
+  ext,
+  simp,
+end
+
+-- 7ª definición
+-- =============
+
+example : (s \ t) ∪ t = s ∪ t :=
+by simp
