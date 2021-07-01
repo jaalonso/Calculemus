ca4c13edfd147abafb0c17d6488cc3f0de976a74
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-07-01 17:17:06 +0200

    Producto_de_potencias_de_la_misma_base_en_monoides.lean

--- src/Producto_de_potencias_de_la_misma_base_en_monoides.lean
+++ src/Producto_de_potencias_de_la_misma_base_en_monoides.lean
@@ -7,12 +7,12 @@
 -- ---------------------------------------------------------------------
 -- En los [monoides](https://en.wikipedia.org/wiki/Monoid) se define la
 -- potencia con exponentes naturales. En Lean la potencia x^n se
--- representa por (npow n x) y se caracteriza por los siguientes lemas:
---    npow_zero' : x ^ 0 = 1
---    npow_succ' : x ^ (succ n) = x * x ^ n
+-- se caracteriza por los siguientes lemas:
+--    pow_zero : x^0 = 1
+--    pow_succ : x^(succ n) = x * x^n
 --
 -- Demostrar que
---    x ^ (m + n) = x ^ m * x ^ n
+--    x^(m + n) = x^m * x^n
 -- ---------------------------------------------------------------------
 
 import algebra.group_power.basic
@@ -29,69 +29,69 @@ set_option pp.structure_projections false
 -- ===============
 
 example :
-  x ^ (m + n) = x ^ m * x ^ n :=
+  x^(m + n) = x^m * x^n :=
 begin
   induction m with m HI,
-  { calc x ^ (0 + n)
-         = x ^ n               : congr_arg ((^) x) (nat.zero_add n)
-     ... = 1 * x ^ n           : (monoid.one_mul (x ^ n)).symm
-     ... = x ^ 0 * x ^ n       : congr_arg (* (x ^ n)) (monoid.npow_zero' x).symm, },
-  { calc x ^ (succ m + n)
-         = x ^ succ (m + n)    : congr_arg ((^) x) (succ_add m n)
-     ... = x * x ^ (m + n)     : pow_succ x (m + n)
-     ... = x * (x ^ m * x ^ n) : congr_arg ((*) x) HI
-     ... = (x * x ^ m) * x ^ n : (monoid.mul_assoc x (x ^ m) (x ^ n)).symm
-     ... = x ^ succ m * x ^ n  : congr_arg (* x^n) (pow_succ x m).symm, },
+  { calc x^(0 + n)
+         = x^n             : congr_arg ((^) x) (nat.zero_add n)
+     ... = 1 * x^n         : (monoid.one_mul (x^n)).symm
+     ... = x^0 * x^n       : congr_arg (* (x^n)) (pow_zero x).symm, },
+  { calc x^(succ m + n)
+         = x^succ (m + n)  : congr_arg ((^) x) (succ_add m n)
+     ... = x * x^(m + n)   : pow_succ x (m + n)
+     ... = x * (x^m * x^n) : congr_arg ((*) x) HI
+     ... = (x * x^m) * x^n : (monoid.mul_assoc x (x^m) (x^n)).symm
+     ... = x^succ m * x^n  : congr_arg (* x^n) (pow_succ x m).symm, },
 end
 
 -- 2ª demostración
 -- ===============
 
 example :
-  x ^ (m + n) = x ^ m * x ^ n :=
+  x^(m + n) = x^m * x^n :=
 begin
   induction m with m HI,
-  { calc x ^ (0 + n)
-         = x ^ n               : by simp only [nat.zero_add]
-     ... = 1 * x ^ n           : by simp only [monoid.one_mul]
-     ... = x ^ 0 * x ^ n       : by simp [monoid.npow_zero'] },
-  { calc x ^ (succ m + n)
-         = x ^ succ (m + n)    : by simp only [succ_add]
-     ... = x * x ^ (m + n)     : by simp only [pow_succ]
-     ... = x * (x ^ m * x ^ n) : by simp only [HI]
-     ... = (x * x ^ m) * x ^ n : (monoid.mul_assoc x (x ^ m) (x ^ n)).symm
-     ... = x ^ succ m * x ^ n  : by simp only [pow_succ], },
+  { calc x^(0 + n)
+         = x^n             : by simp only [nat.zero_add]
+     ... = 1 * x^n         : by simp only [monoid.one_mul]
+     ... = x^0 * x^n       : by simp [pow_zero] },
+  { calc x^(succ m + n)
+         = x^succ (m + n)  : by simp only [succ_add]
+     ... = x * x^(m + n)   : by simp only [pow_succ]
+     ... = x * (x^m * x^n) : by simp only [HI]
+     ... = (x * x^m) * x^n : (monoid.mul_assoc x (x^m) (x^n)).symm
+     ... = x^succ m * x^n  : by simp only [pow_succ], },
 end
 
 -- 3ª demostración
 -- ===============
 
 example :
-  x ^ (m + n) = x ^ m * x ^ n :=
+  x^(m + n) = x^m * x^n :=
 begin
   induction m with m HI,
-  { calc x ^ (0 + n)
-         = x ^ n               : by simp [nat.zero_add]
-     ... = 1 * x ^ n           : by simp
-     ... = x ^ 0 * x ^ n       : by simp, },
-  { calc x ^ (succ m + n)
-         = x ^ succ (m + n)    : by simp [succ_add]
-     ... = x * x ^ (m + n)     : by simp [pow_succ]
-     ... = x * (x ^ m * x ^ n) : by simp [HI]
-     ... = (x * x ^ m) * x ^ n : (monoid.mul_assoc x (x ^ m) (x ^ n)).symm
-     ... = x ^ succ m * x ^ n  : by simp [pow_succ], },
+  { calc x^(0 + n)
+         = x^n             : by simp [nat.zero_add]
+     ... = 1 * x^n         : by simp
+     ... = x^0 * x^n       : by simp, },
+  { calc x^(succ m + n)
+         = x^succ (m + n)  : by simp [succ_add]
+     ... = x * x^(m + n)   : by simp [pow_succ]
+     ... = x * (x^m * x^n) : by simp [HI]
+     ... = (x * x^m) * x^n : (monoid.mul_assoc x (x^m) (x^n)).symm
+     ... = x^succ m * x^n  : by simp [pow_succ], },
 end
 
 -- 4ª demostración
 -- ===============
 
 example :
-  x ^ (m + n) = x ^ m * x ^ n :=
+  x^(m + n) = x^m * x^n :=
 begin
   induction m with m HI,
-  { show x ^ (0 + n) = x ^ 0 * x ^ n,
+  { show x^(0 + n) = x^0 * x^n,
       by simp [nat.zero_add] },
-  { show x ^ (succ m + n) = x ^ succ m * x ^ n,
+  { show x^(succ m + n) = x^succ m * x^n,
       by finish [succ_add,
                  HI,
                  monoid.mul_assoc,
@@ -102,5 +102,5 @@ end
 -- ===============
 
 example :
-  x ^ (m + n) = x ^ m * x ^ n :=
+  x^(m + n) = x^m * x^n :=
 pow_add x m n
