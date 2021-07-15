aa0e83826bf75c4d0115f293ed1853b33d026bfe
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-07-15 11:45:26 +0200

    Limite_multiplicado_por_una_constante

--- src/Limite_multiplicado_por_una_constante.lean
+++ src/Limite_multiplicado_por_una_constante.lean
@@ -67,6 +67,33 @@ begin
     intros ε hε,
     by finish, },
   { intros ε hε,
+    have hc' : 0 < |c| := abs_pos.mpr hc,
+    have hεc : 0 < ε / |c| := div_pos hε hc',
+    specialize h (ε/|c|) hεc,
+    cases h with N hN,
+    use N,
+    intros n hn,
+    specialize hN n hn,
+    dsimp only,
+    calc |c * u n - c * a|
+         = |c * (u n - a)| : congr_arg abs (mul_sub c (u n) a).symm
+     ... = |c| * |u n - a| : abs_mul c  (u n - a)
+     ... < |c| * (ε / |c|) : (mul_lt_mul_left hc').mpr hN
+     ... = ε               : mul_div_cancel' ε (ne_of_gt hc') }
+end
+
+-- 3ª demostración
+-- ===============
+
+example
+  (h : limite u a)
+  : limite (λ n, c * (u n)) (c * a) :=
+begin
+  by_cases hc : c = 0,
+  { subst hc,
+    intros ε hε,
+    by finish, },
+  { intros ε hε,
     have hc' : 0 < |c| := by finish,
     have hεc : 0 < ε / |c| := div_pos hε hc',
     cases h (ε/|c|) hεc with N hN,
