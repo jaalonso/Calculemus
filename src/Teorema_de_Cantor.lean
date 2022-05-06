a3ead827c5df78be2c632757b87e09d37a120527
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-05-06 12:13:46 +0200

    Teorema_de_Cantor

--- src/Teorema_de_Cantor.lean
+++ src/Teorema_de_Cantor.lean
@@ -1,7 +1,7 @@
 -- Teorema_de_Cantor.lean
--- Teorema de Cantor
--- José A. Alonso Jiménez
--- Sevilla, 28 de junio de 2021
+-- Teorema de Cantor.
+-- José A. Alonso Jiménez <https://jaalonso.github.io>
+-- Sevilla, 6-mayo-2022
 -- ---------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------
@@ -19,16 +19,17 @@ variables {α : Type}
 
 example : ∀ f : α → set α, ¬ surjective f :=
 begin
-  intros f surjf,
+  intros f hf,
   let S := {i | i ∉ f i},
-  unfold surjective at surjf,
-  cases surjf S with j fjS,
+  unfold surjective at hf,
+  cases hf S with j hj,
   by_cases j ∈ S,
-  { apply absurd _ h,
-    rw fjS,
+  { dsimp at h,
+    apply h,
+    rw hj,
     exact h, },
   { apply h,
-    rw ← fjS at h,
+    rw ← hj at h,
     exact h, },
 end
 
@@ -37,14 +38,14 @@ end
 
 example : ∀ f : α → set α, ¬ surjective f :=
 begin
-  intros f surjf,
+  intros f hf,
   let S := {i | i ∉ f i},
-  cases surjf S with j fjS,
+  cases hf S with j hj,
   by_cases j ∈ S,
-  { apply absurd _ h,
-    rwa fjS, },
+  { apply  h,
+    rwa hj, },
   { apply h,
-    rwa ← fjS at h, },
+    rwa ← hj at h, },
 end
 
 -- 3ª demostración
@@ -52,17 +53,31 @@ end
 
 example : ∀ f : α → set α, ¬ surjective f :=
 begin
-  intros f surjf,
+  intros f hf,
   let S := {i | i ∉ f i},
-  cases surjf S with j fjS,
+  cases hf S with j hj,
   have h : (j ∈ S) = (j ∉ S), from
     calc  (j ∈ S)
         = (j ∉ f j) : set.mem_set_of_eq
-    ... = (j ∉ S)   : congr_arg not (congr_arg (has_mem.mem j) fjS),
+    ... = (j ∉ S)   : congr_arg not (congr_arg (has_mem.mem j) hj),
   exact false_of_a_eq_not_a h,
 end
 
 -- 4ª demostración
+-- ===============
+
+example : ∀ f : α → set α, ¬ surjective f :=
+begin
+  intros f hf,
+  let S := {i | i ∉ f i},
+  cases hf S with j hj,
+  have h : (j ∈ S) = (j ∉ S),
+  { dsimp,
+    exact congr_arg not (congr_arg (has_mem.mem j) hj), },
+  { exact false_of_a_eq_not_a (congr_arg not (congr_arg not h)), },
+end
+
+-- 5ª demostración
 -- ===============
 
 example : ∀ f : α → set α, ¬ surjective f :=
