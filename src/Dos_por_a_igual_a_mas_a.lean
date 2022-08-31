7850049916d903eefbcf81e024e43961a072a17d
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-31 16:44:47 +0200

    Iniciar Dos_por_a_igual_a_mas_a.lean

---
+++ src/Dos_por_a_igual_a_mas_a.lean
@@ -0,0 +1,16 @@
+-- Dos_por_a_igual_a_mas_a.lean
+-- Si R es un anillo y a ∈ R, entonces 2 * a = a + a.
+-- José A. Alonso Jiménez <https://jaalonso.github.io>
+-- Sevilla, 13-septiembre-2022
+-- ---------------------------------------------------------------------
+
+import algebra.ring
+
+variables {R : Type*} [ring R]
+variables a : R
+
+-- 1ª demostración
+-- ===============
+
+example : 2 + a = a + a :=
+sorry
